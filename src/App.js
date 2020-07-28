3935b91efd6ed172938cdf89e5299a3c7e594bba
Jeovanni <jeovannildg@gmail.com>
2020-07-27 22:20:04 -0600

    changed get request to the app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 import Header from "./Components/Header";
 import PhotoContainer from "./Components/PhotoContainer"
 
 function App() {
-  return (
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?date=2020-07-20&api_key=du2094KoA7O0AlngvxSkX6zMqoaJBeNTyr6oH4Sj')
+      .then(response => {
+          console.log(response)
+      })
+      .catch(error => {
+          console.log("error");
+      })
+  }, [])
+  return (    
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
