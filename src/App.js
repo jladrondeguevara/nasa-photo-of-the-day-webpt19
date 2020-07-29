288a8ed157fce63e7b53bfdeda8c59b7f1af0da8
Jeovanni <jeovannildg@gmail.com>
2020-07-28 21:35:03 -0600

    updated get request

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Header from "./Components/Header";
 import PhotoContainer from "./Components/PhotoContainer"
 
 function App() {
+  // https://api.nasa.gov/planetary/apod?date=2020-07-20&api_key=du2094KoA7O0AlngvxSkX6zMqoaJBeNTyr6oH4Sj
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?date=2020-07-20&api_key=du2094KoA7O0AlngvxSkX6zMqoaJBeNTyr6oH4Sj')
+    axios.get('https://api.nasa.gov/planetary/apod?date=2020-07-28&api_key=du2094KoA7O0AlngvxSkX6zMqoaJBeNTyr6oH4Sj')
       .then(response => {
           console.log(response)
       })
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,13 +4,7 @@ import axios from 'axios';
 
 const Header = () => {
     return (
-        axios.get('https://api.nasa.gov/planetary/apod?date=2020-07-20&api_key=du2094KoA7O0AlngvxSkX6zMqoaJBeNTyr6oH4Sj')
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => {
-            console.log("error");
-        })
+        console.log("header")
     )
 }
 
