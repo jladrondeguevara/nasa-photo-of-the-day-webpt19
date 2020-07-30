3d9b0b75fb2d0df88784357f43a9a5e6ab3ac102
Jeovanni <jeovannildg@gmail.com>
2020-07-29 20:30:07 -0600

    added description and some stylig to the photocontainer

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -5,8 +5,13 @@ import "./PhotoContainer.css";
 export default function PhotoContainer (props) {
     return (
         <div className = "photo-container">
-            <img src = {props.props.hdurl} alt = "Picture of the day"></img>
-            {console.log(props.props.hdurl)}
+            <div className = "photo">
+                <img src = {props.props.hdurl} alt = "Picture of the day"></img>
+                {console.log(props.props)}
+            </div>
+            <div className = "description">
+                <p>{props.props.explanation}</p>
+            </div>
         </div>
     )
 }
